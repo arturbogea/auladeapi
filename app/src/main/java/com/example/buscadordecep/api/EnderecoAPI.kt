f63ead0993c508d621825e2c80c4f5ba9980ce26
arturbogea <arturbogea@gmail.com>
2024-04-10 06:59:47 -0300

    Atualização do app

--- app/src/main/java/com/example/buscadordecep/api/EnderecoAPI.kt
+++ app/src/main/java/com/example/buscadordecep/api/EnderecoAPI.kt
@@ -4,8 +4,9 @@ import retrofit2.http.GET
 
 interface EnderecoAPI {
 
-    //BASE URL: https://viacep.com.br/
-    @GET
+    //BASE URL: https://viacep.com.br/ + ws/01001000/json/
+    //No caso da API via CEP, a rota é onde fica depois do +, pois depois do ws, é onde vai o CEP do lugar que estamos buscando
+    @GET("ws/01001000/json/")
     suspend fun recuperarEndereco()
 
 }
