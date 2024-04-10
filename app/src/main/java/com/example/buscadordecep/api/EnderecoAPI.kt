012016fdb8f46ce32e5b51b7752d8b371a62468c
arturbogea <arturbogea@gmail.com>
2024-04-10 06:55:18 -0300

    Atualização do app

--- app/src/main/java/com/example/buscadordecep/api/EnderecoAPI.kt
+++ app/src/main/java/com/example/buscadordecep/api/EnderecoAPI.kt
@@ -1,7 +1,11 @@
 package com.example.buscadordecep.api
 
+import retrofit2.http.GET
+
 interface EnderecoAPI {
 
+    //BASE URL: https://viacep.com.br/
+    @GET
     suspend fun recuperarEndereco()
 
 }
